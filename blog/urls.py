aaba8ec8fdfe64472e3c960eb60d79f424785c85
george udosen <datameshprojects@gmail.com>
2017-06-26 15:33:38 +0100

    Added post additio from webpage

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
         
         url(r'^$', views.post_list, name='post_list'),
         url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+        url(r'^post/new/$', views.post_new, name='post_new'),
         
         
         ]
